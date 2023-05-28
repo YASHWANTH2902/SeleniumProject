a17263fce6e58430a5878467741569c6112a8eca
YASHWANTH2902 <134403350+YASHWANTH2902@users.noreply.github.com>
2023-05-28 11:22:52 +0530

    commit

---
+++ S1/src/s1/Zomato.java
@@ -0,0 +1,12 @@
+package s1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Zomato {
+public static void main(String[] args) {
+	ChromeDriver driver = new  ChromeDriver();
+	
+	driver.get("http://zomato.com");
+	
+}
+}
